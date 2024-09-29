26d436afcde1fe8c79a19d077cf033036ef16061
KhanmdNazrul <mdnazrulkhandu@gmail.com>
2024-09-29 13:33:26 +0600

    customization

--- wp-content/themes/ariftheme/functions.php
+++ wp-content/themes/ariftheme/functions.php
@@ -1,10 +1,61 @@
 
 <?php
+// function ariftheme_setup(){
+//   add_theme_support( 'post-thumbnails');
+//   add_action('init', 'register_arif_menu');
+// }
 
+add_theme_support( 'post-thumbnails');
 function register_arif_menu() {
   register_nav_menu( 'primary', __( 'Main Menu location', 'ariftheme' ) );
 //   register_nav_menu( 'secondary', __( 'footer Menu location', 'ariftheme' ) );
   
 }
 add_action('after_setup_theme', 'register_arif_menu');
-?>
+
+
+
+
+function slider_custom_post_type() {
+	register_post_type('arif_slider',
+		array(
+			'labels'      => array(
+				'name'          => __('sliders', 'ariftheme'),
+				'singular_name' => __('slider', 'ariftheme'),
+			),
+				'public'      => true,
+				'supports' => array('title','thumbnail')
+		)
+	);
+}
+add_action('init', 'slider_custom_post_type');
+
+
+
+
+function wpdocs_theme_slug_widgets_init() {
+	register_sidebar( array(
+		'name'          => __( 'Right Sidebar', 'ariftheme' ),
+		'id'            => 'sidebar-1',
+		'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'ariftheme' ),
+		'before_widget' => '<div class="box"><div class="heading">',
+		'after_widget'  => '</div></div>',
+		'before_title'  => '',
+		'after_title'   => '',
+	) );
+
+  register_sidebar( array(
+		'name'          => __( 'footer Sidebar-1', 'ariftheme' ),
+		'id'            => 'sidebar-2',
+		'description'   => __( 'Widgets in this area will be shown on all posts and pages.', 'ariftheme' ),
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	) );
+}
+
+add_action( 'widgets_init', 'wpdocs_theme_slug_widgets_init' );
+
+
+
--- wp-content/themes/ariftheme/my_home.php
+++ wp-content/themes/ariftheme/my_home.php
@@ -1,156 +1,146 @@
 ﻿<!DOCTYPE html>
-<html lang="en"> <!--<![endif]-->
+<html lang="en">
+
 <head>
 
     <!-- Basic Page Needs
   ================================================== -->
-	<meta charset="utf-8">
-	<title>zBoomMusic Free Html5 Responsive Template</title>
-	<meta name="description" content="Free Html5 Templates and Free Responsive Themes Designed by Kimmy | zerotheme.com">
-	<meta name="author" content="www.zerotheme.com">
-	
+    <meta charset="utf-8">
+    <title>zBoomMusic Free Html5 Responsive Template</title>
+    <meta name="description" content="Free Html5 Templates and Free Responsive Themes Designed by Kimmy | zerotheme.com">
+    <meta name="author" content="www.zerotheme.com">
+
     <!-- Mobile Specific Metas
   ================================================== -->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
     <!-- CSS
   ================================================== -->
-  	<link rel="stylesheet" href="<?php echo get_template_directory_uri()  ?>/css/zerogrid.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri()  ?>/css/style.css">
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri()  ?>/css/responsive.css">
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri()  ?>/css/responsiveslides.css" />
-	
-	
-	<link href='<?php echo get_template_directory_uri()  ?>/images/favicon.ico' rel='icon' type='image/x-icon'/>
-	<script src="js/jquery.min.js"></script>
-	<script src="js/responsiveslides.js"></script>
-	<script>
-		$(function () {
-		  $("#slider").responsiveSlides({
-			auto: true,
-			pager: false,
-			nav: true,
-			speed: 500,
-			maxwidth: 962,
-			namespace: "centered-btns"
-		  });
-		});
-	</script>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/zerogrid.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/responsive.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/responsiveslides.css" />
+
+
+    <link href='<?php echo get_template_directory_uri(); ?>/./images/favicon.ico' rel='icon' type='image/x-icon' />
+    <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/responsiveslides.js"></script>
+    <script>
+        $(function() {
+            $("#slider").responsiveSlides({
+                auto: true,
+                pager: false,
+                nav: true,
+                speed: 500,
+                maxwidth: 962,
+                namespace: "centered-btns"
+            });
+        });
+    </script>
 </head>
+
 <body>
+    <?php /*Template Name: home_template */ ?>
+    <!--------------Header--------------->
 
-<?php /*Template Name: home_template  */ ?>
+    <?php get_header(); ?>
 
-<!--------------Header--------------->
-<?php get_header(); ?>
 
-<div class="featured">
-	<div class="wrap-featured zerogrid">
-		<div class="slider">
-			<div class="rslides_container">
-				<ul class="rslides" id="slider">
-					<li><img src="<?php echo get_template_directory_uri()  ?>/images/slide1.png"/></li>
-					<li><img src="<?php echo get_template_directory_uri()  ?>/images/slide2.png"/></li>
-					<li><img src="<?php echo get_template_directory_uri()  ?>/images/slide3.png"/></li>
-				</ul>
-			</div>
-		</div>
-	</div>
-</div>
+    <div class="featured">
+        <div class="wrap-featured zerogrid">
+            <div class="slider">
+                <div class="rslides_container">
+                    <ul class="rslides" id="slider">
+                        <?php
+                        $query = new WP_Query(array(
+                            'post_type' => 'arif_slider',
+                        ));
 
-<!--------------Content--------------->
-<section id="content">
-	<div class="wrap-content zerogrid">
-		<div class="row block01">
-			<div class="col-1-3">
-				<div class="wrap-col box">
-					<h2>The White Night</h2>
-					<p>Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis.</p>
-					<div class="more"><a href="#">[...]</a></div>
-				</div>
-			</div>
-			<div class="col-1-3">
-				<div class="wrap-col box">
-					<h2>Tons of Fans</h2>
-					<p>Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis.</p>
-					<div class="more"><a href="#">[...]</a></div>
-				</div>
-			</div>
-			<div class="col-1-3">
-				<div class="wrap-col box">
-					<h2>Best DJ's Ever</h2>
-					<p>Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis.</p>
-					<div class="more"><a href="#">[...]</a></div>
-				</div>
-			</div>
-		</div>
-		<div class="row block02">
-			<div class="col-2-3">
-				<div class="wrap-col">
-					<div class="heading"><h2>Latest Blog</h2></div>
-					<article class="row">
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<img src="<?php echo get_template_directory_uri()  ?>/images/img1.png"/>
-							</div>
-						</div>
-						<div class="col-2-3">
-							<div class="wrap-col">
-								<h2><a href="#">Dreaming With Us All Night</a></h2>
-								<div class="info">By Admin on December 01, 2012 with <a href="#">01 Commnets</a></div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra convallis auctor [...]</p>
-							</div>
-						</div>
-					</article>
-					<article class="row">
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<img src="<?php echo get_template_directory_uri()  ?>/images/img2.png"/>
-							</div>
-						</div>
-						<div class="col-2-3">
-							<div class="wrap-col">
-								<h2><a href="#">Welcome To Our Great Site</a></h2>
-								<div class="info">By Admin on December 01, 2012 with <a href="#">01 Commnets</a></div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra convallis auctor [...]</p>
-							</div>
-						</div>
-					</article>
-					<article class="row">
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<img src="<?php echo get_template_directory_uri()  ?>/images/img3.png"/>
-							</div>
-						</div>
-						<div class="col-2-3">
-							<div class="wrap-col">
-								<h2><a href="#">Stereosonic Is Back Just For You</a></h2>
-								<div class="info">By Admin on December 01, 2012 with <a href="#">01 Commnets</a></div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra convallis auctor [...]</p>
-							</div>
-						</div>
-					</article>
-					<article class="row">
-						<div class="col-1-3">
-							<div class="wrap-col">
-								<img src="<?php echo get_template_directory_uri()  ?>/images/img4.png"/>
-							</div>
-						</div>
-						<div class="col-2-3">
-							<div class="wrap-col">
-								<h2><a href="#">Club Galaxy White Night Show</a></h2>
-								<div class="info">By Admin on December 01, 2012 with <a href="#">01 Commnets</a></div>
-								<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam viverra convallis auctor [...]</p>
-							</div>
-						</div>
-					</article>
-				</div>
-			</div>
-			<?php get_sidebar(); ?>
-		</div>
-	</div>
-</section>
-<!--------------Footer--------------->
-<?php get_footer();  ?>
+                        if ($query->have_posts()):
+                            while ($query->have_posts()):
+                                $query->the_post(); ?>
+                                <li><?php the_post_thumbnail(); ?></li>
+                        <?php
+                            endwhile;
+                        endif;
+                        ?>
 
-</body></html>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--------------Content--------------->
+    <section id="content">
+        <div class="wrap-content zerogrid">
+            <div class="row block01">
+                <div class="col-1-3">
+                    <div class="wrap-col box">
+                        <h2>The White Night</h2>
+                        <p>Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis.</p>
+                        <div class="more"><a href="#">[...]</a></div>
+                    </div>
+                </div>
+                <div class="col-1-3">
+                    <div class="wrap-col box">
+                        <h2>Tons of Fans</h2>
+                        <p>Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis.</p>
+                        <div class="more"><a href="#">[...]</a></div>
+                    </div>
+                </div>
+                <div class="col-1-3">
+                    <div class="wrap-col box">
+                        <h2>Best DJ's Ever</h2>
+                        <p>Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis.</p>
+                        <div class="more"><a href="#">[...]</a></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row block02">
+                <div class="col-2-3">
+                    <div class="wrap-col">
+                        <div class="heading">
+                            <h2>Latest Blog</h2>
+                        </div>
+                        <?php
+                        $query = new WP_Query(array(
+                            'post_type' => 'post',
+							'posts_per_page' => 5,
+                        ));
+                        if ($query->have_posts()) {
+                            while ($query->have_posts()) {
+                                $query->the_post();
+                        ?>
+                                <article class="row">
+                                    <div class="col-1-3">
+                                        <div class="wrap-col">
+                                            <?php the_post_thumbnail(); ?>
+                                        </div>
+                                    </div>
+                                    <div class="col-2-3">
+                                        <div class="wrap-col">
+                                            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                                            <div class="info">[By <?php the_author(); ?> on <?php the_date('F d, Y'); ?><a href="#"> 01 Commnets</a>]</div>
+                                            <p><?php the_excerpt(  ); ?></p>
+                                        </div>
+                                    </div>
+                                </article>
+                        <?php
+
+                            }
+                        }
+                        ?>
+
+                    </div>
+                </div>
+                <?php get_sidebar(); ?>
+            </div>
+        </div>
+    </section>
+    <!--------------Footer--------------->
+    <?php get_footer(); ?>
+
+</body>
+
+</html>
